cb8d1feb11a7747759f429cd1c0f4228abe4996b
smlrkh1107 <krae7789230@gmail.com>
2020-04-21 20:22:42 +0900

    Id저장여부를 기록하는 기능 추가(체크박스 선택 유무)  - ContextUtil 클래스에 항목 추가

--- app/src/main/java/kun/hee/sharedpreferences/MainActivity.kt
+++ app/src/main/java/kun/hee/sharedpreferences/MainActivity.kt
@@ -2,6 +2,7 @@ package kun.hee.sharedpreferences
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kun.hee.sharedpreferences.Utils.ContextUtil
 
@@ -15,14 +16,23 @@ class MainActivity : BaseActivity() {
 
 
     override fun setupEvents() {
-        val inputId = idEdt.text.toString()
-        ContextUtil.setUserId(mContext, inputId)
+        idSaveCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
+            ContextUtil.setSaveId(mContext, isChecked)
+            if (isChecked) Toast.makeText(mContext, "아이디를 저장합니다.",Toast.LENGTH_SHORT).show()
+            else Toast.makeText(mContext, "아이디 저장을 해제합니다.", Toast.LENGTH_SHORT).show()
+        }
+
+
+        loginBtn.setOnClickListener {
+            val inputId = idEdt.text.toString()
+            ContextUtil.setUserId(mContext, inputId)
+        }
 
     }
 
     override fun setValues() {
-
         idEdt.setText(ContextUtil.getUserId(mContext)) //editText는  settext쓰고, textview는 .text = 함.
+        idSaveCheckBox.isChecked = ContextUtil.getSaveId(mContext)
 
     }
 
--- app/src/main/java/kun/hee/sharedpreferences/Utils/ContextUtil.kt
+++ app/src/main/java/kun/hee/sharedpreferences/Utils/ContextUtil.kt
@@ -9,13 +9,13 @@ class ContextUtil {
 
         val prefName = "MyPref" // id를 받아 저장할 ""파일명""
         val USER_ID = "USER_ID" // 저장될 항목의 이름.(그 사람 코드? 그사람이 사용하는 정보(id)XXX)
+        val ID_SAVE = "ID_SAVE"
 
 //        setter:데이터 저장용 함수
         fun setUserId (context: Context, inputId:String){ //★메모장 파일을 여는 행위
             val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
                                 // 저장할 파일을 열어라(파일명, 우리앱전용으로쓸께)
             // USER_ID항목에 함수의 재료로 들어오는 String 변수 저장.
-
             pref.edit().putString(USER_ID, inputId).apply() //아이디를 입력받은 값으로 저장(apply)해 주겠다.
         }
 
@@ -25,6 +25,17 @@ class ContextUtil {
             return pref.getString(USER_ID, "")!!
 
         }
+
+        fun setSaveId(context: Context, isSave:Boolean){
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            pref.edit().putBoolean(ID_SAVE, isSave).apply()
+        }
+
+        fun getSaveId(context: Context) :Boolean {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            return pref.getBoolean(ID_SAVE,false)
+        }
+
 
     }
 }
